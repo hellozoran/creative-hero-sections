80971854107450e432ff63bb9f0e05295fd35152
Zoran Mitrovic <32069164+hellozoran@users.noreply.github.com>
2022-12-22 11:42:52 -0600

    Testing fetch in functions

--- functions/send.js
+++ functions/send.js
@@ -1,8 +1,17 @@
-import axios from 'axios'
-
 export async function handler(event, context) {
-  return axios.post('https://hooks.zapier.com/hooks/catch/4353581/b742gwn/', event.body)
-    .then(() => ({
+  const url = 'https://hooks.zapier.com/hooks/catch/4353581/b742gwn/'
+  const options = {
+    method: 'POST',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    body: JSON.stringify(event.body),
+  }
+
+  return fetch(url, options)
+    .then((response) => response.json())
+    .then((data) => ({
       statusCode: 200,
       body: JSON.stringify({ message: 'Successfully sent message.' })
     }))
@@ -12,4 +21,16 @@ export async function handler(event, context) {
         body: JSON.stringify(err)
       }
     })
+
+  // return axios.post('https://hooks.zapier.com/hooks/catch/4353581/b742gwn/', event.body)
+  //   .then(() => ({
+  //     statusCode: 200,
+  //     body: JSON.stringify({ message: 'Successfully sent message.' })
+  //   }))
+  //   .catch((err) => {
+  //     return {
+  //       statusCode: 500,
+  //       body: JSON.stringify(err)
+  //     }
+  //   })
 }
