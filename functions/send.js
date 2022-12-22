d89091297509267ad112fffe3a888d41a6eda46c
Zoran Mitrovic <32069164+hellozoran@users.noreply.github.com>
2022-12-22 11:47:49 -0600

    removed axios code from functions

--- functions/send.js
+++ functions/send.js
@@ -21,16 +21,4 @@ export async function handler(event, context) {
         body: JSON.stringify(err)
       }
     })
-
-  // return axios.post('https://hooks.zapier.com/hooks/catch/4353581/b742gwn/', event.body)
-  //   .then(() => ({
-  //     statusCode: 200,
-  //     body: JSON.stringify({ message: 'Successfully sent message.' })
-  //   }))
-  //   .catch((err) => {
-  //     return {
-  //       statusCode: 500,
-  //       body: JSON.stringify(err)
-  //     }
-  //   })
 }
