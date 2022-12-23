30c3f1ea68b217b967dc4a315e2a31efafd3fd08
Zoran Mitrovic <32069164+hellozoran@users.noreply.github.com>
2022-12-23 14:55:57 -0600

    removed spa import in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import PrerenderSPAPlugin from 'prerender-spa-plugin'
 
 export default defineConfig({
   plugins: [
