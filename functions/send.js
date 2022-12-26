5a26e2230a4dca649097d3bf2c43880962b3961f
Zoran Mitrovic <32069164+hellozoran@users.noreply.github.com>
2022-12-26 13:12:03 -0600

    Updated contact form

--- functions/send.js
+++ functions/send.js
@@ -1,43 +1,22 @@
-// export async function handler(event, context) {
-  
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify(event.body)
-//   }
-//   return fetch(url, options)
-//     .then((response) => response.json())
-//     .then((data) => ({
-//       statusCode: 200,
-//       body: JSON.stringify({ message: 'Successfully sent message.' })
-//     }))
-//     .catch((err) => {
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify(err)
-//       }
-//     })
-// }
+import axios from 'axios'
 
+const url = 'https://hooks.zapier.com/hooks/catch/4353581/b742gwn/'
 
 export async function handler(event, context) {
-  const url = 'https://hooks.zapier.com/hooks/catch/4353581/b742gwn/'
-  const options = {
-    method: 'POST',
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    body: JSON.stringify(event.body),
+  try { 
+    await axios.post(url, event.body) 
   }
-  try {
-    const response = await fetch(url, options)
-    const data = await response.json();
-    return { statusCode: 200, body: JSON.stringify({ data }) };
-  } catch (error) {
-    console.log(error);
+  catch (err) {
     return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed sending data' }),
-    };
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
   }
-};
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data: 'Successfully sent.'})
+  }
+}
